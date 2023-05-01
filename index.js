9cea5954b06c3c1c8804c8ac7bbc1fc94298bada
d.khasenov <d.khasenov@aviata.me>
2023-05-01 18:29:34 +0600

    feat: complete backend for blog mern

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import multer from 'multer';
+import { registerValidation, loginValidation, postCreateValidation } from './validations/index.js';
+import { UserController, PostController } from './controllers/index.js';
+import { checkAuth, handleValidationErrors } from './utils/index.js';
 
-const app = express()
+mongoose
+  .connect(
+    'mongodb+srv://khassenov:khassenov22@cluster0.4yeftnl.mongodb.net/blog-mern?retryWrites=true&w=majority'
+  )
+  .then(() => console.log('DB ok💥'))
+  .catch((err) => console.log('DB error', err));
 
-app.get('/', (req, res) => {
-	res.send('Hello world')
-})
+const app = express();
+
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+app.use(express.json());
+app.use('/uploads', express.static('uploads'));
+
+app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
+app.post('/auth/signin', registerValidation, handleValidationErrors, UserController.signin);
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`
+  });
+});
+
+app.get('/posts', PostController.getAll);
+app.get('/posts/:id', PostController.getOne);
+app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.deletePost);
+app.patch(
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
 
 app.listen(3000, (err) => {
-	if (err) {
-		return console.warn(err)
-	}
-	console.log('Server is starting...')
-})
+  if (err) {
+    return console.warn(err);
+  }
+  console.log('Server is started...');
+});
---
+++ models/Post.js
@@ -0,0 +1,33 @@
+import { Schema, model } from 'mongoose';
+
+const PostSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+    tags: {
+      type: Array,
+      default: []
+    },
+    viewsCount: {
+      type: Number,
+      default: 0
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    imageUrl: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+export default model('Post', PostSchema);
